30dd206a0e026870e605bae0df9ef1571f08dd0c
Tommy Cheang <tommycheang91@gmail.com>
2020-02-26 23:46:19 -0800

    Added brute force two sum method

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,18 @@
+import com.leetcode.easy.TwoSum;
 
+import java.util.Arrays;
+import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		TwoSum twoSumDriver = new TwoSum();
+		int [] testcase_1 = new int [] {2, 7, 11, 15};
+		int target = 9;
+		int [] testcase_result;
+		testcase_result = twoSumDriver.twoSum(testcase_1, target);
+		System.out.println(Arrays.toString(testcase_result));
 	}
 
 }
